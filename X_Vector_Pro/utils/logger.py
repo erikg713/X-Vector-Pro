b6e4dfeb2ebd7c000e5a7adb23654a3b6bceac5b
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 08:41:52 -0500

    Update logger.py

--- X_Vector_Pro/utils/logger.py
+++ X_Vector_Pro/utils/logger.py
@@ -1,1 +1,11 @@
+from datetime import datetime
 
+def log(msg):
+    with open("xvector_log.txt", "a") as f:
+        f.write(f"[{datetime.now()}] {msg}\n")
+
+def save_hit(username, password):
+    hit = f"{username}:{password}"
+    with open("hits.txt", "a") as f:
+        f.write(f"{hit}\n")
+    log(f"[+] Valid credentials found: {hit}")
