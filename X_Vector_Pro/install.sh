5523460e168b3aac2a7c64682b3119db23717e4a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 08:43:12 -0500

    Update install.sh

--- X_Vector_Pro/install.sh
+++ X_Vector_Pro/install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+echo "[*] Installing X-Vector Pro dependencies..."
+sudo apt update
+sudo apt install python3-tk -y
+pip install requests customtkinter tldextract
+echo "[*] Done. Run with: ./run.sh"
