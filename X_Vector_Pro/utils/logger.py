35547f4123333e0aff18e4c6c0fa9a0552c0364a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 09:56:17 -0500

    Update logger.py

--- X_Vector_Pro/utils/logger.py
+++ X_Vector_Pro/utils/logger.py
@@ -3,6 +3,13 @@ from datetime import datetime
 def log(msg):
     with open("xvector_log.txt", "a") as f:
         f.write(f"[{datetime.now()}] {msg}\n")
+    print(msg)
+
+from datetime import datetime
+
+def log(msg):
+    with open("xvector_log.txt", "a") as f:
+        f.write(f"[{datetime.now()}] {msg}\n")
 
 def save_hit(username, password):
     hit = f"{username}:{password}"
