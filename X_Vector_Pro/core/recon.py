56b8547afacc12562d2bec30bc2733d2f70f7910
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 08:45:51 -0500

    Update recon.py

--- X_Vector_Pro/core/recon.py
+++ X_Vector_Pro/core/recon.py
@@ -1,1 +1,19 @@
+import requests
+import socket
+from utils.logger import log
 
+def basic_recon(target_url):
+    log("[*] Starting recon...")
+
+    try:
+        ip = socket.gethostbyname(target_url.replace("http://", "").replace("https://", "").split("/")[0])
+        headers = requests.get(target_url).headers
+        server = headers.get("Server", "Unknown")
+        powered_by = headers.get("X-Powered-By", "Unknown")
+    except Exception as e:
+        log(f"[!] Recon failed: {e}")
+        return
+
+    log(f"[+] IP Address: {ip}")
+    log(f"[+] Server: {server}")
+    log(f"[+] X-Powered-By: {powered_by}")
