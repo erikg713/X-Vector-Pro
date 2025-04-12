e10aca2b378c788a440cf3701604ae72c342952e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-12 08:40:38 -0500

    Update controller.py

--- X_Vector_Pro/core/controller.py
+++ X_Vector_Pro/core/controller.py
@@ -1,1 +1,6 @@
+from core.brute import xmlrpc_brute
+from utils.logger import log
 
+def start_brute_force(url, username):
+    log("[*] Launching XML-RPC brute force...")
+    xmlrpc_brute(url, username)
