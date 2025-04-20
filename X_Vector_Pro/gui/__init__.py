4fd3265dc330e47d15c370fb580e022271edd8f4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-20 01:30:44 -0500

    Update __init__.py

--- X_Vector_Pro/gui/__init__.py
+++ X_Vector_Pro/gui/__init__.py
@@ -1,1 +1,20 @@
 # gui package for X-Vector Pro interface
+# gui/__init__.py
+
+from .dashboard import Dashboard
+from .tabs.brute_tab import BruteTab
+from .tabs.recon_tab import ReconTab
+from .tabs.scanner_tab import ScannerTab
+from .tabs.ids_tab import IDSTab
+from .tabs.exploit_tab import ExploitTab
+from .tabs.auto_mode_tab import AutoModeTab
+
+__all__ = [
+    "Dashboard",
+    "BruteTab",
+    "ReconTab",
+    "ScannerTab",
+    "IDSTab",
+    "ExploitTab",
+    "AutoModeTab"
+]
