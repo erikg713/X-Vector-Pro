da1a2a35497772804ebac86245a1ed8d7aeca6f8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-21 08:02:52 -0500

    Create __init__.py

---
+++ X_Vector_Pro/gui/tabs/full_auto/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py for the full_auto module
+
+from .auto_recon import run_auto_recon  # Makes the auto_recon function available when importing full_auto
+
+# You can also add any common configurations or utilities here if needed in the future
+from gui.tabs.full_auto import run_auto_recon
