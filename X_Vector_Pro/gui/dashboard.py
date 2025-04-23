50bcad33d65e511e4c30c800179390dd8aa80082
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-22 23:10:06 -0500

    Update dashboard.py

--- X_Vector_Pro/gui/dashboard.py
+++ X_Vector_Pro/gui/dashboard.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import customtkinter as ctk
 import tkinter.filedialog as fd
 from pymongo import MongoClient
