610e1ea8e8805e84143c3e902562b1f91d121e52
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-25 15:39:29 -0500

    Update __init__.py

--- X_Vector_Pro/utils/__init__.py
+++ X_Vector_Pro/utils/__init__.py
@@ -1,1 +1,4 @@
-# utils package for X-Vector Pro
+# X_Vector_Pro/utils/__init__.py
+
+# You can expose commonly used utilities here
+from .logger import log, encrypt_log  # assuming encrypt_log is defined there
