dda75064cb2f7b153ac14adbb4488432ff1bcca3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-25 15:37:44 -0500

    Update logger.py

--- X_Vector_Pro/utils/logger.py
+++ X_Vector_Pro/utils/logger.py
@@ -1,4 +1,14 @@
 from datetime import datetime
+from cryptography.fernet import Fernet
+
+def encrypt_log(data: str, output_path: str):
+      key = Fernet.generate_key()
+      cipher_suite = Fernet(key)
+      encrypted_data = cipher_suite.encrypt(data.encode())
+
+      with open(output_path, 'wb') as f:
+          f.write(encrypted_data)
+
 
 def log(msg):
     with open("xvector_log.txt", "a") as f:
