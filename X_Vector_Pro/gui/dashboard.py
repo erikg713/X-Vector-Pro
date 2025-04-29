a608fbe85ec2e775666259204302b3e0332346e9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-29 02:12:22 -0500

    Update dashboard.py

--- X_Vector_Pro/gui/dashboard.py
+++ X_Vector_Pro/gui/dashboard.py
@@ -5,7 +5,13 @@ import sys
 import importlib
 import customtkinter as ctk
 import json
+import os
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+HITS_FILE = os.path.join(BASE_DIR, "logs", "hits.txt")
+SESSION_FILE = os.path.join(BASE_DIR, "logs", "session.json")
+LOG_FILE = os.path.join(BASE_DIR, "logs", "xvector_log.txt")
+WORDLIST_DIR = os.path.join(BASE_DIR, "wordlists")
 # Load config.json
 DATA_DIR = os.path.dirname(__file__)
 CONFIG_PATH = os.path.join(DATA_DIR, 'config.json')
