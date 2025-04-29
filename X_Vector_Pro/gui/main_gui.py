2373dfebd44b190223690bba68f14c9bb8395bea
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-29 02:12:52 -0500

    Update main_gui.py

--- X_Vector_Pro/gui/main_gui.py
+++ X_Vector_Pro/gui/main_gui.py
@@ -9,7 +9,13 @@ import xmlrpc.client
 import itertools
 import tkinter as tk
 from datetime import datetime
+import os
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+HITS_FILE = os.path.join(BASE_DIR, "logs", "hits.txt")
+SESSION_FILE = os.path.join(BASE_DIR, "logs", "session.json")
+LOG_FILE = os.path.join(BASE_DIR, "logs", "xvector_log.txt")
+WORDLIST_DIR = os.path.join(BASE_DIR, "wordlists")
 # === Setup ===
 log_file_path = "xvector_log.txt"
 session_file = "session.json"
