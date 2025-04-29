c97df99afe2fa58bc34bee4180d1f8b673b0fae8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-04-29 02:10:06 -0500

    Update base_tab.py

--- X_Vector_Pro/gui/base_tab.py
+++ X_Vector_Pro/gui/base_tab.py
@@ -1,3 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+HITS_FILE = os.path.join(BASE_DIR, "logs", "hits.txt")
+SESSION_FILE = os.path.join(BASE_DIR, "logs", "session.json")
+LOG_FILE = os.path.join(BASE_DIR, "logs", "xvector_log.txt")
+WORDLIST_DIR = os.path.join(BASE_DIR, "wordlists")
+
 import customtkinter as ctk
 
 class BaseTab(ctk.CTkFrame):
