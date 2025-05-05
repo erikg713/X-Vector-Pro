3a28f7661c6e3810fc1a202a9ddd7cf09a598660
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-05 14:58:03 -0500

    Update __init__.py

--- core/auto_mode/__init__.py
+++ core/auto_mode/__init__.py
@@ -1,4 +1,11 @@
-from core.auto_mode import run_sequence
+"""
+core.auto_mode
+--------------
+
+This package provides the functionality to run automated sequences
+in the X-Vector Pro Supreme cybersecurity toolkit.
+"""
+
 from .sequencer import run_sequence
 
 __all__ = ["run_sequence"]
