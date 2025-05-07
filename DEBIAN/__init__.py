976f8e31b0dcc114ddb92c8456b05ae427a1a976
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-07 13:32:32 -0500

    Update __init__.py

--- DEBIAN/__init__.py
+++ DEBIAN/__init__.py
@@ -1,1 +1,4 @@
+# DEBIAN/__init__.py
 
+# Empty file or a placeholder to mark the DEBIAN directory as a Python package
+# In most cases, DEBIAN is used for packaging information and may not require an init script
