0883637236d296a8ecbf223501be9fe2af8be6a0
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-10 08:04:28 -0500

    Update __init__.py

--- gui/widgets/__init__.py
+++ gui/widgets/__init__.py
@@ -1,1 +1,19 @@
+# gui/widgets/__init__.py
 
+from .stealth_button import StealthButton
+from .dark_mode_toggle import DarkModeToggle
+from .toast_notifier import ToastNotifier
+from .loading_indicator import LoadingIndicator
+from .status_bar import StatusBar
+from .sidebar_nav import SidebarNav
+from .invisible_widget import InvisibleWidget
+
+__all__ = [
+    "StealthButton",
+    "DarkModeToggle",
+    "ToastNotifier",
+    "LoadingIndicator",
+    "StatusBar",
+    "SidebarNav",
+    "InvisibleWidget",
+]
