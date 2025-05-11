d5cd1bd20bce313a1962efde25a26acd446c0dc7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-11 00:13:06 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,5 @@
-# utils/logger.py
-
 from core import logger
-
+from datetime import datetime 
 def log(message):
     logger.info(message)
 
@@ -10,3 +8,8 @@ def warn(message):
 
 def error(message):
     logger.error(message)
+
+def log_to_central(msg, logs_output):
+    timestamp = datetime.now().strftime("%H:%M:%S")
+    logs_output.insert("end", f"[{timestamp}] {msg}\n")
+    logs_output.see("end")  # auto-scroll
