8aaf713e96e622e1eed303cb1a6ada07f5ed084f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-30 17:42:50 -0500

    Update setup_logger.py

--- utils/setup_logger.py
+++ utils/setup_logger.py
@@ -1,46 +1,36 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-import logging
-import os
 
-def create_logger(name="XVectorPro", log_level=logging.INFO, log_file=None):
-    logger = logging.getLogger(name)
-    logger.setLevel(log_level)
-
-    # Prevent duplicate handlers if this function is called multiple times
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(log_level)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    # File handler (optional)
-    if log_file:
-        os.makedirs(os.path.dirname(log_file), exist_ok=True)
-        fh = logging.FileHandler(log_file)
-        fh.setLevel(log_level)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-
-    return logger
-
-def create_logger(log_dir="logs", log_file="xvector.log", max_bytes=1_000_000, backup_count=5):
+def create_logger(
+    name="XVectorPro",
+    log_level=logging.INFO,
+    log_dir="logs",
+    log_file="xvector.log",
+    max_bytes=1_000_000,
+    backup_count=5,
+    console_output=True
+):
     os.makedirs(log_dir, exist_ok=True)
     path = os.path.join(log_dir, log_file)
 
-    logger = logging.getLogger("XVectorLogger")
-    logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
 
-    if not logger.handlers:
-        handler = RotatingFileHandler(path, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+    if console_output:
+        ch = logging.StreamHandler()
+        ch.setLevel(log_level)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    fh = RotatingFileHandler(path, maxBytes=max_bytes, backupCount=backup_count)
+    fh.setLevel(log_level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
     return logger
